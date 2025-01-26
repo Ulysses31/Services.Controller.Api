53bf8573920c6aeb8b7c282ab9bfb9528307c578
SLG\ciordanidis <ciordanidis@singularlogic.eu>
2025-01-26 12:39:17 +0200

    add app options documentation

--- Configuration/CommonAppOptions.cs
+++ Configuration/CommonAppOptions.cs
@@ -1,19 +1,54 @@
-namespace Services.Controllers.API.Configuration;
-
-/// <summary>
-/// CommonAppOptions class
-/// </summary>
-public class CommonAppOptions
+namespace Services.Controllers.API.Configuration
 {
-  public const string AppOptions = "ApplicationSettings";
+  /// <summary>
+  /// Represents configuration options for the application.
+  /// This class provides constants and static properties for managing application settings.
+  /// </summary>
+  public class CommonAppOptions
+  {
+    /// <summary>
+    /// The name of the application settings section in the configuration file.
+    /// </summary>
+    public const string AppOptions = "ApplicationSettings";
 
-  public static string? AppName { get; set; }
-  public static string? AppVersion { get; set; }
-  public static string? AppDescription { get; set; }
-  public static string? AppUrl { get; set; }
-  public static bool EnableRateLimiting { get; set; }
-  public static bool EnableHealthCheck { get; set; }
-  public static bool EnableApiCache { get; set; }
-  public static bool EnableGenApiClient { get; set; }
+    /// <summary>
+    /// Gets or sets the name of the application.
+    /// </summary>
+    public static string? AppName { get; set; }
 
+    /// <summary>
+    /// Gets or sets the version of the application.
+    /// </summary>
+    public static string? AppVersion { get; set; }
+
+    /// <summary>
+    /// Gets or sets the description of the application.
+    /// </summary>
+    public static string? AppDescription { get; set; }
+
+    /// <summary>
+    /// Gets or sets the URL of the application.
+    /// </summary>
+    public static string? AppUrl { get; set; }
+
+    /// <summary>
+    /// Indicates whether rate limiting is enabled for the application.
+    /// </summary>
+    public static bool EnableRateLimiting { get; set; }
+
+    /// <summary>
+    /// Indicates whether health checks are enabled for the application.
+    /// </summary>
+    public static bool EnableHealthCheck { get; set; }
+
+    /// <summary>
+    /// Indicates whether API caching is enabled for the application.
+    /// </summary>
+    public static bool EnableApiCache { get; set; }
+
+    /// <summary>
+    /// Indicates whether the generation of an API client is enabled for the application.
+    /// </summary>
+    public static bool EnableGenApiClient { get; set; }
+  }
 }
